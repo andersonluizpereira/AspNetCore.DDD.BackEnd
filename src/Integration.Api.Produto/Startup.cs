4e82ffbf48e099c3cf3db06a62155e8d80d2cc0d
andersonluizpereira <andy2903.alp@gmail.com>
2017-11-18 16:46:45 -0200

    implementando swagger para exibição API's

--- src/Integration.Api.Produto/Startup.cs
+++ src/Integration.Api.Produto/Startup.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Integration.Ioc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Integration.Api.Produto
 {
@@ -24,6 +28,36 @@ namespace Integration.Api.Produto
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                             new Info
+                             {
+                                 Title = "Integrador de produtos",
+                                 Version = "v1",
+                                 Description = "API REST criada com o ASP.NET Core, para integração de produtos",
+                                 Contact = new Contact
+                                 {
+                                     Name = "Andy",
+                                     Url = "https://github.com/andersonluizpereira/AspNetCore.DDD.BanckEnd"
+                                 }
+                             });
+
+                string caminhoAplicacao =
+                    PlatformServices.Default.Application.ApplicationBasePath;
+                string nomeAplicacao =
+                    PlatformServices.Default.Application.ApplicationName;
+                string caminhoXmlDoc =
+                    Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+
+                c.IncludeXmlComments(caminhoXmlDoc);
+            });
+
+
+            // .NET Native DI Abstraction
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +69,18 @@ namespace Integration.Api.Produto
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                                  "Integrador de produtos");
+            });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            // Adding dependencies from another layers (isolated from Presentation)
+            SimpleInjectorBootStrapper.RegisterServices(services);
         }
     }
 }
