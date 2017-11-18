10efb7a4204b847206648d22cd1c040bd487af1c
andersonluizpereira <andy2903.alp@gmail.com>
2017-11-18 15:22:56 -0200

    Implementando API e Map Aplicação

---
+++ Integration.Infra.Data/Mapping/Produtos/ProdutoMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Integration.Domain.Entities.Models;
+using Integration.Infra.Data.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Integration.Infra.Data.Mapping.Produtos
+{
+    public class ProdutoMap : EntityTypeConfiguration<ProdutosViewModel>
+    {
+        public override void Map(EntityTypeBuilder<ProdutosViewModel> builder)
+        {
+            builder.ToTable("Produtos");
+
+            builder.Property(c => c.Id)
+                   .HasColumnType("int")
+                   .IsRequired();
+
+            builder.Property(c => c.Descricao)
+                   .HasColumnType("varchar(150)")
+                   .HasMaxLength(150);
+
+            builder.Property(c => c.Preco)
+                   .HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
